231fea66df36a6a7880accc17101b31ba8494ebb
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-10-06 00:30:18 +0300

    InMemoryDb service+added scoped ICommandRepo

--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -1,8 +1,19 @@
+using CommandsService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseInMemoryDatabase("InMem");
+});
+
+builder.Services.AddScoped<ICommandRepo, CommandRepo>();
+
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
