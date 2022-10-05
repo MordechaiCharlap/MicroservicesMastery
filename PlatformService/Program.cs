bc814f82eb8081ed2db8aa2eb511b9367fa9ca64
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-10-05 18:10:34 +0300

    change original build to fix migration bugs

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,14 +1,9 @@
+using System.Data.Common;
 using Microsoft.EntityFrameworkCore;
 using PlatformService.Data;
 using PlatformService.SyncDataServices.Http;
 
 var builder = WebApplication.CreateBuilder(args);
-
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 
 Console.WriteLine($"--> CommandsService endpoint: {builder.Configuration["CommandsService"]}");
 
@@ -17,16 +12,19 @@ if (builder.Environment.IsProduction())
     Console.WriteLine("--> Using SqlServer");
 
     var connString = builder.Configuration.GetConnectionString("PlatformConn");
-    connString += $"Password={builder.Configuration["saPassword"]}";
+    connString += $"Password={builder.Configuration["saPassword"]};";
 
     builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(connString));
 }
 else
 {
     Console.WriteLine("--> Using InMem Db");
-    builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMemDb"));
+    builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
 }
 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
@@ -44,7 +42,7 @@ if (app.Environment.IsDevelopment())
 }
 
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
