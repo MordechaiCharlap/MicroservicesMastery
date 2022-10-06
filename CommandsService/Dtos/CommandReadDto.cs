fd9643a34e3e0b3122c425787092015ebd4bfaa7
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-10-06 14:38:52 +0300

    stagedChanges

--- CommandsService/Dtos/CommandReadDto.cs
+++ CommandsService/Dtos/CommandReadDto.cs
@@ -2,7 +2,7 @@ namespace CommandsService.Dtos
 {
     public class CommandReadDto
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string HowTo { get; set; }
         public string CommandLine { get; set; }
         public int PlatformId { get; set; }
